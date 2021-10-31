8a99893b2d5b26143ce81b555967e3d321f22cea
Ho3einzargar <Ho3einzargar@yahoo.com>
2021-10-31 15:16:21 +0330

    createclass

--- Javalearn2(final).js
+++ Javalearn2(final).js
@@ -1,2 +1,29 @@
-alert("salam");
-alert("ss");
+class User{
+constructor(username , password){
+    this.username=prompt("Hello pls enter username");
+    if(this.username=="",this.username=='username'){
+        alert("pls enter your username");
+        this.username=prompt("Hello pls enter username");
+    }
+    else{
+        this.password=prompt("Hello pls enter password");
+
+    }
+
+
+}
+sayuser(){
+    alert( "username : " + `${this.username}` + "  password : " + `${this.password}`);   
+}
+
+
+}
+let user = new User();
+user.sayuser();
+
+
+
+
+
+
+
